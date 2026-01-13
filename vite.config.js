9759b4f07c19a480896efd8d4f0421657ae5a612
diwash1437 <diwashlimbu47@gmail.com>
2026-01-13 16:28:58 +0900

    image add and path on vite.config name base for github

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/honten_restaurant_website/', // <-- Add this line
+// The reason for the blank page: 
+// Vite uses absolute paths by default (/) for JS/CSS files.
+//  GitHub Pages hosts it under /honten_restaurant_website/, 
+// so it can't find your scripts and styles, leading to a blank page.
+
 })
