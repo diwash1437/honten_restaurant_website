1ab31a4809036cc552fd1f4c6ba9c3576277d845
diwash1437 <diwashlimbu47@gmail.com>
2026-01-13 23:04:21 +0900

    link changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,24 @@
-
-// import './App.css'
-import NameChange from './components/NameChange';
-import ProductCard from './components/ProductCard';
-import ProductList from './components/ProductList';
-import NameChangeTry from './components/NameChangeTry';
-import { AlcholOrder } from './page/AlcholOrder';
-import { PickurusuRecipe } from './Recipe/PickurusuRecipe/PickurusuRecipe';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Restaurant from "./Recipe/Restaurant";
+import { PickurusuRecipe } from './Recipe/PickurusuRecipe/PickurusuRecipe';
+import { AlcholOrder } from './page/AlcholOrder';
 import { VegetableSoup } from './Recipe/VegetableSoup';
 import { VegetableSoupOrder } from './order/VegetableSoupOrder';
-import {Lunch} from './premium/Lunch'
+import { Lunch } from './premium/Lunch';
 import NabataColtd153 from './order/NabataColtd153';
+
 function App() {
- 
-
-  return(
+  return (
     <Routes>
-
-
-   {/* <div> */}
-    {/* <h1>Shopping Cart</h1> */}
-    {/* <ProductList/> */}
-    {/* <ProductCard/> */}
-    {/* <NameChangeTry/> */}
-    {/* <NameChangeTry/> */}
-    {/* <AlcholOrder/> */}
-    {/* {<PickurusuRecipe/>} */}
-   {/* </div> */}
-
-
-
-   <Route path='honten_restaurant_website/' element={<Restaurant/>}/>
-   <Route path='/pickurusu' element={<PickurusuRecipe />}/>
-   <Route path='/alcholOrder' element={<AlcholOrder/>}/>
-  <Route path='/vegetableSoup' element={<VegetableSoup/>}/>
-  <Route path='/vegetableSoupOrder' element={<VegetableSoupOrder/>}/>
-  <Route path='/lunch' element={<Lunch/>}/>
-  <Route path='honten_restaurant_website/nabata' element={<NabataColtd153/>}/>
-  </Routes>
-  )
+      <Route path="/" element={<Restaurant />} />
+      <Route path="/pickurusu" element={<PickurusuRecipe />} />
+      <Route path="/alcholOrder" element={<AlcholOrder />} />
+      <Route path="/vegetableSoup" element={<VegetableSoup />} />
+      <Route path="/vegetableSoupOrder" element={<VegetableSoupOrder />} />
+      <Route path="/lunch" element={<Lunch />} />
+      <Route path="/nabata" element={<NabataColtd153 />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/honten_restaurant_website">
     <App />
   </BrowserRouter>
 )
